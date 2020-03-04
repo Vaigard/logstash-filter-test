8fc4bf8125efb98f6fc8be16a4259106c9ab1df2
Vaigard <Vaigard>
2020-03-04 23:37:31 +0300

    http server skeleton

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # logstash-filter-test
 Http-server testing Logstash filters
+
+Works on port 8081.
+
+Pages:
+- /ping
+- /upload
---
+++ http-server/main.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"io"
+	"io/ioutil"
+	"net/http"
+)
+
+// "/"
+// Main page return documentation about server.
+func mainHandler(responseWriter http.ResponseWriter, request *http.Request) {
+	readmeContent, err := ioutil.ReadFile("../README.md")
+    if err == nil {
+    	io.WriteString(responseWriter, string(readmeContent))
+	} else {
+		io.WriteString(responseWriter, "Logstash filters tester's server\n")
+	}
+}
+
+// "/ping"
+// Allow simply check if server works.
+func pingHandler(responseWriter http.ResponseWriter, request *http.Request) {
+	io.WriteString(responseWriter, "pong")
+}
+
+// "/upload"
+// Gets the logstash filter and testing data.
+func logstashPipelineHandler(responseWriter http.ResponseWriter, request *http.Request) {
+	io.WriteString(responseWriter, "logstashPipelineHandler\n")
+}
+
+func main() {
+	http.HandleFunc("/", mainHandler)
+	http.HandleFunc("/ping", pingHandler)
+	http.HandleFunc("/upload", logstashPipelineHandler)
+
+	http.ListenAndServe(":8081", nil)
+}
