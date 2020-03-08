19879cf0aaa9ab1668a19ce2f1d136b2eb681410
Vaigard <vaigard03117@gmail.com>
2020-03-08 23:39:26 +0300

    Skeleton of http-server application. (#1)
    * http server skeleton
    
    * fix indention
    
    * fix indention
    
    * fix README path
    
    * add log.Fatal to server listen call
    
    * Sample response.
    Input and output types.
    
    * convert tabs to spaces
    
    * add checkRequest func stub
    
    * Add request checking
    
    * some fixes
    
    * delete debug logging
    
    * Receive multipart data and parse it in getRequestType function
    
    * Modify getRequestType to getPipelineInput: return parsed request
    
    * example of resuest in README
    
    * Stubs of pipeline processing functions.
    
    * Fix procesing logic.
    
    Co-authored-by: Vaigard <Vaigard>

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
 # logstash-filter-test
 Http-server testing Logstash filters
+
+Works on port 8081.
+
+Pages:
+- /ping
+- /upload
+
+Example of upload request:
+```
+curl -i --request POST -F "filter=@/home/user/filter.conf" -F 'message="testhost testtext"' -F 'expected="{\"name\":\"hostname\",\"message\":\"testtext\"}"' 127.0.0.1:8081/upload
+```
---
+++ http-server/main.go
@@ -0,0 +1,159 @@
+package main
+
+import (
+    "io"
+    "io/ioutil"
+    "net/http"
+    "log"
+    "encoding/json"
+    "bytes"
+)
+
+const (
+    RequestTypeInvalid                 = -1
+    RequestTypeEmptyFilter             = 0
+    RequestTypeMessageFilter           = 1
+    RequestTypeMessageFilterExpected   = 2
+    RequestTypeFilter                  = 3
+)
+
+type logstashPipelineInput struct {
+    Message     string
+    Filter      string
+    Expected    string
+}
+
+type logstashPipelineOutput struct {
+    Output      string      `json:"output"`
+    Diff        string      `json:"diff"`
+    Lint        string      `json:"lint"`
+    Status      string      `json:"status"`
+}
+
+func lintFitler(filter string) string {
+    return "lintFitler"
+}
+
+func processMessage(message string, filter string) string {
+    return "processMessage"
+}
+
+func compareOutput(expected string, actual string) string {
+    return "compareOutput"
+}
+
+func testPipeline(pipelineInput logstashPipelineInput, requestType int) logstashPipelineOutput {
+    if requestType == RequestTypeFilter {
+        return logstashPipelineOutput{Lint: lintFitler(pipelineInput.Filter)}
+    }
+
+    pipelineOutput := logstashPipelineOutput{Output: processMessage(pipelineInput.Message, pipelineInput.Filter)}
+
+    if requestType == RequestTypeMessageFilterExpected {
+        pipelineOutput.Diff = compareOutput(pipelineInput.Expected, pipelineOutput.Output)
+    }
+
+    return pipelineOutput
+}
+
+func getPipelineInput(request *http.Request) (logstashPipelineInput, int) {
+    pipelineInput := logstashPipelineInput{}
+    multiPartReader, error := request.MultipartReader()
+    if error != nil {
+        return pipelineInput, RequestTypeInvalid
+    }
+
+    for {
+        part, error := multiPartReader.NextPart()
+
+        // This is OK, no more parts
+        if error == io.EOF {
+            break
+        }
+
+        // Any other error
+        if error != nil {
+            return pipelineInput, RequestTypeInvalid
+        }
+
+        var buffer bytes.Buffer
+        io.Copy(&buffer, part)
+
+        switch part.FormName() {
+        case "filter":
+            pipelineInput.Filter = buffer.String()
+        case "message":
+            pipelineInput.Message = buffer.String()
+        case "expected":
+            pipelineInput.Expected = buffer.String()
+        default:
+            return pipelineInput, RequestTypeInvalid
+        }
+    }
+
+    if pipelineInput.Filter == "" {
+        return pipelineInput, RequestTypeEmptyFilter
+    }
+
+    if pipelineInput.Message != "" && pipelineInput.Expected != "" {
+        return pipelineInput, RequestTypeMessageFilterExpected
+    }
+
+    if pipelineInput.Message != "" && pipelineInput.Expected == "" {
+        return pipelineInput, RequestTypeMessageFilter
+    }
+
+    return pipelineInput, RequestTypeFilter
+}
+
+// "/"
+// Main page returns documentation about server.
+func mainHandler(responseWriter http.ResponseWriter, request *http.Request) {
+    readmeContent, error := ioutil.ReadFile("README.md")
+    if error == nil {
+        io.WriteString(responseWriter, string(readmeContent))
+    } else {
+        io.WriteString(responseWriter, "Logstash filters tester's server\n")
+    }
+}
+
+// "/ping"
+// Allow simply check if server works.
+func pingHandler(responseWriter http.ResponseWriter, request *http.Request) {
+    io.WriteString(responseWriter, "pong")
+}
+
+// "/upload"
+// Gets the logstash filter and testing data.
+func logstashPipelineHandler(responseWriter http.ResponseWriter, request *http.Request) {
+    pipelineOutput := logstashPipelineOutput{}
+
+    pipelineInput, requestType := getPipelineInput(request)
+
+    switch requestType {
+    case RequestTypeInvalid:
+        pipelineOutput = logstashPipelineOutput{Status: "Invalid request"}
+    case RequestTypeEmptyFilter:
+        pipelineOutput = logstashPipelineOutput{Status: "Empty filter field"}
+    default:
+        pipelineOutput = testPipeline(pipelineInput, requestType)
+        pipelineOutput.Status = "OK"
+    }
+
+    responseJson, marshalError := json.Marshal(pipelineOutput)
+    if marshalError != nil {
+        http.Error(responseWriter, marshalError.Error(), http.StatusInternalServerError)
+        return
+    }
+
+    responseWriter.Header().Set("Content-Type", "application/json")
+    responseWriter.Write(responseJson)
+}
+
+func main() {
+    http.HandleFunc("/", mainHandler)
+    http.HandleFunc("/ping", pingHandler)
+    http.HandleFunc("/upload", logstashPipelineHandler)
+
+    log.Fatal(http.ListenAndServe(":8081", nil))
+}
