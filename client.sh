2fc7d6b093bfe901c0ea2246b21bad2820083799
Vaigard <Vaigard>
2020-04-25 16:53:00 +0300

    upd client.sh

--- client.sh
+++ client.sh
@@ -1,17 +1,21 @@
 #!/bin/sh
 
-if [ "$#" -ne 6 ]; then
-  echo "Usage: ./client.sh -s <server> -f <filter file name> -m <message file name>" >&2
+if [ "$#" -ne 6 ] & [ "$#" -ne 10 ]; then
+  echo "Usage: ./client.sh -s <server> -f <filter file name> -m <message file name> [-p <patterns file name> -d <patterns directories>]" >&2
   exit 1
 fi
 
-while getopts ":f:m:s:" opt; do
+while getopts ":f:m:s:p:d:" opt; do
   case $opt in
   	s) server="$OPTARG"
     ;;
     f) filter_file="$OPTARG"
     ;;
     m) message_file="$OPTARG"
+    ;;
+    p) patterns_file="$OPTARG"
+    ;;
+    d) patterns_dir="$OPTARG"
     ;;
     \?) echo "Invalid option: -$OPTARG" >&2 && exit 1
     ;;
@@ -40,4 +44,8 @@ if [ "$ping_res" != "pong" ]; then
 	exit 1
 fi
 
-curl --request POST -F "filter=@$filter_file" -F "message=@$message_file" "$server"/upload && echo
+if [ ! -f "$patterns_file" ]; then
+  curl --request POST -F "filter=@$filter_file" -F "message=@$message_file" "$server"/upload && echo
+else
+  curl --request POST -F "filter=@$filter_file" -F "message=@$message_file" -F "patterns=@$patterns_file" -F "patterns_dir=$patterns_dir" "$server"/upload && echo
+fi
